66b9bc2360ae91559672690118de177f298a3a27
meteorologia <gastonlopez5@gmail.com>
2020-03-30 18:48:24 -0300

    El broadcast se inicia solamente cuando se conecta el celular al usb

--- app/src/main/java/com/example/tpi_1/USBconectado.java
+++ app/src/main/java/com/example/tpi_1/USBconectado.java
@@ -3,12 +3,18 @@ package com.example.tpi_1;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
 public class USBconectado extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Intent i = new Intent(context, InicialLlamada.class);
-        context.startActivity(i);
+        if(intent.getExtras().getBoolean("connected")){
+            Intent i = new Intent(context, InicialLlamada.class);
+            context.startActivity(i);
+        } else {
+            Toast.makeText(context, "No conectado", Toast.LENGTH_LONG).show();
+        }
+
     }
 }
